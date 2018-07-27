f3bd59d6ded899372bcc978a3027578b4c95c475
john <johnjacobcosgrove@gmail.com>
2018-07-27 14:03:59 -0400

    fixed delete button

--- scripts/bookmark.js
+++ scripts/bookmark.js
@@ -19,7 +19,7 @@ const bookmarks = (function(){
             </div>
         </li>`
         }
-        else{
+        else {
             return `
             <li class="bookmark-view large" data-bookmark-id="${bookmark.id}">
             <div class="bookmark-box">
@@ -36,8 +36,8 @@ const bookmarks = (function(){
                 <button class="delete-button">delete</button>
             </div>
         </li>`
-        }
-    }
+        };
+    };
 
     const generateBookmarkString = function(bookmarkList){
         const items = bookmarkList.map(generateBookmark);
@@ -48,11 +48,11 @@ const bookmarks = (function(){
         let items = store.items;
         if(store.filterValue){
             items = items.filter(item => item.rating >= store.filterValue)
-        }
-        const bookmarkString = generateBookmarkString(items)
+        };
+        const bookmarkString = generateBookmarkString(items);
 
         $('.bookmark-list').html(bookmarkString);
-    }
+    };
 
 
 
@@ -66,7 +66,7 @@ const bookmarks = (function(){
         $('.bookmark-list').on('click', 'button.edit-button', event=>{
             const id = getItemIdFromElement(event.currentTarget);
             const bookmark = store.findById(id);
-            $(bookmark).copyToForm('.add-bookmark-form')
+            $(bookmark).copyToForm('.add-bookmark-form');
             store.editId = id;
         })
     }
@@ -75,8 +75,8 @@ const bookmarks = (function(){
         $('.bookmark-title').val('');
         $('.bookmark-description').val('');
         $('.bookmark-url').val('');
-        $('.bookmark-rating').val('1')
-        $('.error-message').text('')
+        $('.bookmark-rating').val('1');
+        $('.error-message').text('');
     }
 
 
@@ -91,37 +91,36 @@ const bookmarks = (function(){
                 clearValues();
                 render();
             },() => {
-                $('.error-message').text('Title and URL are required')})
+                $('.error-message').text('Title and URL are required')});
         }
-            else{
+            else {
                 event.preventDefault();
                 const newBookmark = $(event.target).serializeJson();
-                const oldBookmark = store.findById(store.editId)
-                const newBookmarkObject = JSON.parse(newBookmark)
+                const oldBookmark = store.findById(store.editId);
+                const newBookmarkObject = JSON.parse(newBookmark);
                 
                 api.editBookmark(newBookmark, () =>{
                     
-                    store.findAndUpdate(oldBookmark,newBookmarkObject)
-                    clearValues()
+                    store.findAndUpdate(oldBookmark,newBookmarkObject);
+                    clearValues();
                     render();
                 }, ()=> {
                     $('.error-message').text('Title and URL are required')
-                })
-                }
-        })
-    }
+                });
+                };
+        });
+    };
 
     const handleDeleteBookmark = function(){
         $('.bookmark-list').on('click', 'button.delete-button', event =>{
             console.log('ran')
             const id = getItemIdFromElement(event.currentTarget);
             api.deleteBookmark(id, () =>{
-                console.log(id)
                 store.findAndDelete(id);
                 render();
-            })
-        })
-    }
+            });
+        });
+    };
 
     const handleRatingFilter = function(){
         $('.bookmark-rating-filter').change(function(){
